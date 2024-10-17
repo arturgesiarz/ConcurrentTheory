e8ebc316bf7ebe0cd567055d32232afa6ce3486a
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-10-17 19:17:42 +0200

    first two excrecises done

--- Lab01/src/main/java/org/agh/Main.java
+++ Lab01/src/main/java/org/agh/Main.java
@@ -1,27 +1,52 @@
 package org.agh;
 
 public class Main {
-    public static void main(String[] args) {
-        // TODO
+    public static void main(String[] args) throws InterruptedException {
         // ex 1 - witchout synchronized
-//        Counter counter = new Counter();
-//
-//        IncrementThread incrementThread = new IncrementThread(counter);
-//        DecrementThread decrementThread = new DecrementThread(counter);
-//
-//        Thread thread1 = new Thread(incrementThread);
-//        Thread thread2 = new Thread(decrementThread);
-//
-//        thread1.start();
-//        thread2.start();
-//
-//        thread1.join();
-//        thread2.join();
-//
-//        System.out.println(counter.getX());
-//
-//        // ex 2 - witch synchronized
-//        System.out.println(counter.getX());
+
+        int INCREMENT_TIMES = 100000000;
+        Counter counter = new Counter(0);
+
+        Thread incrementThreadNonSynchronized = new Thread(() -> {
+            for (int i = 0; i < INCREMENT_TIMES; i++) {
+                counter.increment();
+            }
+        });
+        Thread decrementThreadNonSynchronized = new Thread(() -> {
+            for (int i = 0; i < INCREMENT_TIMES; i++) {
+                counter.decrement();
+            }
+        });
+
+        incrementThreadNonSynchronized.start();
+        decrementThreadNonSynchronized.start();
+
+        incrementThreadNonSynchronized.join();
+        decrementThreadNonSynchronized.join();
+
+        System.out.println("Ex1: Non synchronized result: " + counter.getSecretValue());
+
+        // ex 2 - witch synchronized
+        counter.setSecretValue(0);
+
+        Thread incrementThreadSynchronized = new Thread(() -> {
+            for (int i = 0; i < INCREMENT_TIMES; i++) {
+                counter.synchronizedIncrement();
+            }
+        });
+        Thread decrementThreadSynchronized = new Thread(() -> {
+            for (int i = 0; i < INCREMENT_TIMES; i++) {
+                counter.synchronizedDecrement();
+            }
+        });
+
+        incrementThreadSynchronized.start();
+        decrementThreadSynchronized.start();
+
+        incrementThreadSynchronized.join();
+        decrementThreadSynchronized.join();
+
+        System.out.println("Ex2: Synchronized result: " + counter.getSecretValue());
 //
 //        // ex 3
 //        Buffer buffer = new Buffer();
