3a1bfe093e53bfb71900175fd795cfa74493289f
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-11-08 09:03:18 +0100

    QS

--- Lab04/src/main/java/org/agh/fair/producer/consumer/FairProducerConsumer.java
+++ Lab04/src/main/java/org/agh/fair/producer/consumer/FairProducerConsumer.java
@@ -6,15 +6,19 @@ import java.io.IOException;
 public class FairProducerConsumer {
     private static void go(int bufferSize, int numberOfWorkers, FileWriter fileWriter)
         throws IOException, InterruptedException {
+
         Buffer buffer = new Buffer(bufferSize, fileWriter);
         Thread[] workers = new Thread[numberOfWorkers * 2];
+
         for (int i = 0; i < numberOfWorkers; i++) {
             workers[i] = new Thread(new Producer(buffer, bufferSize));
             workers[i + numberOfWorkers] = new Thread(new Consumer(buffer, bufferSize));
         }
+
         for (int i = 0; i < 2 * numberOfWorkers; i++) {
             workers[i].start();
         }
+
         for (int i = 0; i < 2 * numberOfWorkers; i++) {
             workers[i].join();
         }
