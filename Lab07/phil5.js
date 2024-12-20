b72a8234afe0d95b5c4fb06752d998de9be6bf15
Artur Gęsiarz <arturgesiarz@student.agh.edu.pl>
2024-12-20 10:10:06 +0100

    CSVs have been created

--- Lab07/phil5.js
+++ Lab07/phil5.js
@@ -20,25 +20,53 @@ var Fork = function () {
   return this;
 };
 
-Fork.prototype.acquire = function (cb) {
+Fork.prototype.acquire = function (cb, time = 1) {
   // zaimplementuj funkcje acquire, tak by korzystala z algorytmu BEB
   // (http://pl.wikipedia.org/wiki/Binary_Exponential_Backoff), tzn:
   // 1. przed pierwsza proba podniesienia widelca Filozof odczekuje 1ms
   // 2. gdy proba jest nieudana, zwieksza czas oczekiwania dwukrotnie
   //    i ponawia probe itd.
-  let delay = 1;
-
-  const attemptAcquire = () => {
-    if (this.state === 0) {
+  setTimeout(() => {
+    if (this.state == 0) {
       this.state = 1;
       cb();
     } else {
-      delay *= 2;
-      setTimeout(attemptAcquire, delay);
+      this.acquire(cb, time * 2);
+    }
+  }, time);
+};
+
+// Acquire for conductor
+Fork.prototype.acquireLeft = function (cb, philosophersNumber) {
+  var self = this;
+
+  var check = function (time) {
+    if (self.state === 0) {
+      if (acquiredForks >= philosophersNumber - 1) {
+        setTimeout(check, 1, 1);
+      } else {
+        self.state = 1;
+        cb();
+      }
+    } else {
+      setTimeout(check, time, time * 2);
     }
   };
 
-  setTimeout(attemptAcquire, delay);
+  setTimeout(check, 1, 1);
+};
+
+Fork.prototype.acquireRight = function (cb) {
+  var self = this;
+  var check = function (time) {
+    if (self.state === 0) {
+      self.state = 1;
+      cb();
+    } else {
+      setTimeout(check, time, time * 2);
+    }
+  };
+  setTimeout(check, 1, 1);
 };
 
 Fork.prototype.release = function () {
@@ -112,19 +140,19 @@ Philosopher.prototype.startAsym = function (count, philosophersNumber) {
   // zaimplementuj rozwiazanie asymetryczne
   // kazdy filozof powinien 'count' razy wykonywac cykl
   // podnoszenia widelcow -- jedzenia -- zwalniania widelcow
-  const eat = (iteration) => {
+  eat = (iteration) => {
     if (iteration >= count) {
       console.log(`Filozof ${id} zakonczyl jedzenie!`);
       return;
     }
 
     console.log(`Filozof ${id} probuje podniesc widelce.`);
-    var start = new Date();
+    var start = new Date().getTime();
     forks[left].acquire(() => {
       console.log(`Filozof ${id} podniósł widelec ${left}.`);
 
       forks[right].acquire(() => {
-        appendToCSV("phil_asym.csv", philosophersNumber, start);
+        // appendToCSV("phil_asym.csv", philosophersNumber, start);
         console.log(`Filozof ${id} podniósł widelec ${right}`);
 
         console.log(`Filozof ${id} je.`);
@@ -144,89 +172,45 @@ Philosopher.prototype.startAsym = function (count, philosophersNumber) {
   eat(0);
 };
 
-Philosopher.prototype.startConductor = function (
-  count,
-  conductor,
-  philosophersNumber
-) {
+Philosopher.prototype.startConductor = function (count, philosophersNumber) {
   var forks = this.forks,
     f1 = this.f1,
     f2 = this.f2,
     id = this.id;
 
-  // zaimplementuj rozwiazanie z kelnerem
-  // kazdy filozof powinien 'count' razy wykonywac cykl
-  // podnoszenia widelcow -- jedzenia -- zwalniania widelcow
-  const eat = (iteration) => {
+  eat = function (iteration) {
     if (iteration >= count) {
       console.log(`Filozof ${id} zakonczyl jedzenie!`);
       return;
     }
 
-    console.log(
-      `Filozof ${id} prosi kelnera o zgodę na podniesienie widelców.`
-    );
-    var start = new Date();
-    conductor.requestPermission(() => {
-      appendToCSV("phil_conductor.csv", philosophersNumber, start);
-      console.log(`Kelner wyraził zgodę dla filozofa ${id}.`);
+    start = new Date();
+    console.log(`Filozof ${id} czeka na pozwolenie lokaja.`);
 
-      forks[f1].acquire(() => {
-        console.log(`Filozof ${id} podniósł widelec ${f1}.`);
+    forks[f1].acquireLeft(() => {
+      acquiredForks += 1;
 
-        forks[f2].acquire(() => {
-          console.log(`Filozof ${id} podniósł widelec ${f2}`);
+      forks[f2].acquireRight(() => {
+        acquiredForks += 1;
+        console.log(`Filozof ${id} dostal pozwolenie lokaja.`);
+        // appendToCSV("phil_conductor.csv", philosophersNumber, start);
 
-          console.log(`Filozof ${id} je.`);
-          setTimeout(() => {
-            forks[f1].release();
-            console.log(`Filozof ${id} zwolnił widelec ${f1}.`);
+        console.log(`Filozof ${id} je.`);
+        setTimeout(() => {
+          forks[f1].release();
+          console.log(`Filozof ${id} zwolnil widelec ${f1}.`);
 
-            forks[f2].release();
-            console.log(`Filozof ${id} zwolnił widelec ${f2}.`);
+          forks[f2].release();
+          console.log(`Filozof ${id} zwolnil widelec ${f2}.`);
 
-            conductor.releaseForks();
-
-            eat(iteration + 1);
-          }, 100);
-        });
+          acquiredForks -= 2;
+          eat(iteration + 1);
+        }, 100);
       });
-    });
+    }, philosophersNumber);
   };
 
   eat(0);
-};
-
-// Conductor class
-var Conductor = function (forksNumber) {
-  this.waitingPhilosphers = [];
-  this.avaiableForks = forksNumber;
-  return this;
-};
-
-Conductor.prototype.requestPermission = function (cb) {
-  var avaiableForks = this.avaiableForks;
-
-  if (avaiableForks >= 2) {
-    avaiableForks -= 2;
-    this.avaiableForks = avaiableForks;
-    cb();
-  } else {
-    this.waitingPhilosphers.push(cb);
-  }
-};
-
-Conductor.prototype.releaseForks = function () {
-  var waitingPhilosphers = this.waitingPhilosphers;
-  var avaiableForks = this.avaiableForks;
-  avaiableForks += 2;
-
-  while (avaiableForks >= 2 && waitingPhilosphers.length > 0) {
-    avaiableForks -= 2;
-    this.avaiableForks = avaiableForks;
-    const cb = waitingPhilosphers.shift();
-    cb();
-  }
 };
 
 // TEST - Functions
@@ -274,29 +258,17 @@ const test_phil_conductor = function (
   for (var i = 0; i < philosophersNumber; i++) {
     forks.push(new Fork());
   }
-  var conductor = new Conductor(forks.length);
 
   for (var i = 0; i < philosophersNumber; i++) {
     philosophers.push(new Philosopher(i, forks));
   }
 
   for (var i = 0; i < philosophersNumber; i++) {
-    philosophers[i].startConductor(
-      eatingCounter,
-      conductor,
-      philosophersNumber
-    );
+    philosophers[i].startConductor(eatingCounter, philosophersNumber);
   }
 };
 
 // Normal Test
-// test_phil_naive(5);
-// test_phil_asym(6);
-// test_phil_conductor(5);
-
-// Speed Test
-let philosophersTests = [5, 6, 7, 8, 9, 10];
-philosophersTests.forEach((philosophers) => {
-  test_phil_asym(philosophers);
-  test_phil_conductor(philosophers);
-});
+test_phil_naive(5);
+test_phil_asym(5);
+test_phil_conductor(5);
